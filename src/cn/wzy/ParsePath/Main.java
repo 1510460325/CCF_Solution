37594c77938d35f17f863166bde917a49e4a3e53
1510460325@qq.com <1510460325@qq.com>
2018-09-09 16:43:52 +0800

    外加单源最短路径dijistra的题解

---
+++ src/cn/wzy/ParsePath/Main.java
@@ -0,0 +1,18 @@
+package cn.wzy.ParsePath;
+
+import java.util.Scanner;
+
+/**
+ * Create by Wzy
+ * on 2018/7/26 9:43
+ * 不短不长八字刚好
+ */
+public class Main {//路径解析
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 15;
+        n = scanner.nextInt();
+        System.out.println(n);
+    }
+}
